3420192c3346e7bb78d4da6c04fd42f132084015
AgusUruguayo <agusca6@gmail.com>
2023-05-15 15:33:40 -0300

    -

--- bot.js
+++ bot.js
@@ -9,12 +9,12 @@ bot.use((ctx, next) => {
 
 bot.start((ctx) => {
   console.log(`
-  📩 ============== Información del Mensaje ==============
-  🙋‍♂️ Enviado por: ${ctx.from.first_name} ${ctx.from.last_name}
-  👤 ID de Usuario: ${ctx.from.id}
-  📨 Mensaje: "${ctx.message.text}"
-  📩 Tipo de Mensaje: [${ctx.updateSubTypes[0]}]
-  =============================================
+  📩  ============== Información del Mensaje ==============
+  🙋‍♂️  Enviado por: ${ctx.from.first_name} ${ctx.from.last_name}
+  👤  ID de Usuario: ${ctx.from.id}
+  📨  Mensaje: "${ctx.message.text}"
+  📩  Tipo de Mensaje: [${ctx.updateSubTypes[0]}]
+   =============================================
   `);
 
   bot.telegram.sendMessage(ctx.chat.id, "**¡Hola mundo!**", {
